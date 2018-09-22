6144079d7e555c14ab30dd5483a24e03de87d348
Alexandre Henrique Kavalerski Teixeira <kavalerskialexandre@gmail.com>
2018-09-21 21:17:52 -0300

    Criando Admin de Pessoa e Aluno

--- gamep/core/admin.py
+++ gamep/core/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
-
+from core.models import *
 # Register your models here.
 
+
+@admin.register(Pessoa)
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email')
+    raw_id_fields = ('usuario', )
+
+
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    search_fields = ('nome_pessoa', )
+    list_display = ('nome_pessoa', )
+    raw_id_fields = ('pessoa', )
+
+    def nome_pessoa(self, obj):
+        return obj.pessoa.nome
+
+    nome_pessoa.admin_order_field = 'pessoa__nome'
+    nome_pessoa.__name__ = 'Nome'
