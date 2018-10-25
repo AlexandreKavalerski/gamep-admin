94162cc7e84a90ee4d5141ed79896e340af5bfbf
Alexandre Henrique Kavalerski Teixeira <kavalerskialexandre@gmail.com>
2018-10-25 18:50:00 -0300

    last one

--- gamep/settings.py
+++ gamep/settings.py
@@ -134,7 +134,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
 STATIC_URL = 'static/'
 
 
