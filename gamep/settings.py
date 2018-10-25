9a36c915c68f1a0b99bf88f5a341bef94ec656e1
Alexandre Henrique Kavalerski Teixeira <kavalerskialexandre@gmail.com>
2018-10-25 18:06:47 -0300

    Corrigindo caminho de midias

--- gamep/settings.py
+++ gamep/settings.py
@@ -129,17 +129,19 @@ USE_L10N = True
 USE_TZ = True
 
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '{}/static/'.format(URL_PREFIX)
 
 
 MEDIA_URL = '{}/media/'.format(URL_PREFIX)
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '{}/static'.format(URL_PREFIX)),
+    os.path.join(PROJECT_ROOT, '{}/static'.format(URL_PREFIX)),
     'resultados/templates/static',
 ]
 
